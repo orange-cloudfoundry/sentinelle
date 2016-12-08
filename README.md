93a04f12e449f37d18209ab390b06437787a8fc4
Sebastien Bortolussi <sebastien.bortolussi@orange.com>
2016-12-08 18:12:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# sentinelle
+
+Whether it is for troubleshooting or monitoring, you may need to get information about connectivity from 
+your CF Space. Sentinelle provides Ops through an HTTP API with common network commands, such as :
+
+- ping
+- traceroute
+- tcpdump
+
---
+++ src/main/java/com/orange/ops/sentinelle/SentinelleApplication.java
@@ -0,0 +1,12 @@
+package com.orange.ops.sentinelle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentinelleApplication.class, args);
+	}
+}
